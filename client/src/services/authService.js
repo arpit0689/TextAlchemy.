4b2da80486f5b9ba65f52b8fef1fcfd0fe4dc989
Arpit Pandey <arpit0689@gmail.com>
2026-05-13 15:34:25 +0530

    fixed api routes

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,7 +1,7 @@
 import api from "./api.js";
 
 export const loginUser = async (payload) => {
-  const { data } = await api.post("/auth/login", payload);
+  const { data } = await api.post("api/auth/login", payload);
   return data;
 };
 
